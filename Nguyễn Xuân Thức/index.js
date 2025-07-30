2742f66527ad74a3e23c6dfbcd9c4287dbfcad91
thielang <xuanthuc123412@gmail.com>
2025-07-30 17:36:19 +0700

    Xuan Thuc sended homework ss1

---
+++ Nguyễn Xuân Thức/index.js
@@ -0,0 +1,8 @@
+import utils from './utils.js';
+const {productInfo, greet, sumUpTo, square, isEven} = utils;
+
+console.log(productInfo("Sach", 20000));
+console.log(greet("An"));
+console.log(sumUpTo(5));
+console.log(square(6));
+console.log(isEven(4));
