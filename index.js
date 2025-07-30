467eaad8a7957754b61018ad5a2cdf805bc12ad5
Bach-de-SherlockHomes <vietbach1601@gmail.com>
2025-07-30 20:29:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+// // Bài 1
+// import { productInfo } from "./utils.js";
+// productInfo("Sách", 20000);
+// productInfo("Vo", 30000);
+
+// // Bài 2
+// import { greet } from "./utils.js";
+// let tenNguoi = prompt("Nhập tên")
+// greet(tenNguoi);
+
+// // Bài 3
+// import { sumUpTo } from "./utils.js";
+// let n = +prompt("Nhập n")
+// alert(sumUpTo(n));
+
+// // Bài 4
+// import { square } from "./utils.js";
+// let n = +prompt("Nhập n")
+// alert(square(n));
+
+// // Bài 5
+// import { isEven } from "./utils.js";
+// let n = +prompt("Nhập n")
+// isEven(n);
+
+// // Bài 6
+// import { firstElement } from "./utils.js";
+// let n = [1, 2, 3, 4, 5]
+// firstElement(n);
+
+// // Bài 7
+// import { sum } from "./utils.js";
+// sum(1, 2, 3, 4, 6);
+
+// // Bài 8
+// import { extractObject } from "./utils.js";
+// const user = { name: "Bách", age: 21 };
+// console.log(extractObject(user));
+
+// // Bài 9
+// import { extractArrayObject } from "./utils.js";
+// const people = [{ name: "And" }, { name: "Bình" }, { name: "Bách" }];
+// extractArrayObject(people);
+
+// // Bài 10
+// import { stringLength } from "./utils.js";
+// let n = prompt("Nhập chuỗi")
+// stringLength(n);
+
+// // Bài 11
+// import { toUpperCase } from "./utils.js";
+// let n = prompt("Nhập chuỗi")
+// console.log(toUpperCase(n));
+
+// // Bài 12
+// import { formatCurrency } from "./utils.js";
+// let n = +prompt("Nhập tiền")
+// console.log(formatCurrency(n));
+// // Bài 13
+// import { max } from "./utils.js";
+// let myArray = [10, 5, 20, 8];
+// console.log(max(myArray));
+// // Bài 14
+// import { isPrime } from "./utils.js";
+// isPrime(6)
+// // Bài 15
+// import { repeatString } from "./utils.js";
+// console.log(repeatString('hello', 3))
+// // Bài 16
+// import { sumOdd } from "./utils.js";
+// console.log(sumOdd([1, 2]))
+// // Bài 17
+// import { sumGreaterThan } from "./utils.js";
+// console.log(sumGreaterThan([1, 2]))
+// // Bài 18
+// import { formatDate } from "./utils.js";
+// console.log(formatDate('2024-11-28'))
+// // Bài 19
+// import { firstThree } from "./utils.js";
+// console.log(firstThree([1, 2, 3, 4, 5]))
+// // Bài 20
+// import { containsChar } from "./utils.js";
+// console.log(containsChar("javascript", "a"))
+// // Bài 22
+// import { countOccurrences } from "./utils.js";
+// console.log(countOccurrences([1, 2, 2, 3, 2], 2))
+// // Bài 25
+// import { factorial } from "./utils.js";
+// console.log(factorial(5))
+// // Bài 26
+// import { separateEvenOdd } from "./utils.js";
+// console.log(separateEvenOdd([1, 2, 3, 4]))
+// // Bài 27
+// import { sortAsc } from "./utils.js";
+// console.log(sortAsc([1, 5, 100, 4]))
+// // Bài 28
+// import { sumEvenIndex } from "./utils.js";
+// console.log(sumEvenIndex([1, 5, 100, 4]))
+// // Bài 29
+// import { multiplyByTwo } from "./utils.js";
+// console.log(multiplyByTwo([1, 5, 100, 4]))
+// Bài 30
+import { arrayToString } from "./utils.js";
+console.log(arrayToString(["a", "b", "c"]))
